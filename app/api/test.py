85710ab51d0cd6f89c753177cdf0ff9c82faf300
Jon Huber <hubermjonathan@gmail.com>
2020-10-10 19:46:24 -0700

    fixed test data

--- app/api/test.py
+++ app/api/test.py
@@ -53,96 +53,96 @@ def create_test_data():
 
     db.users.create_user(
         connection, 'player1', 'player1-email1@gmail.com', '12195751591')  # user_id = 3
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 3, 'player1-phone2')  # add phone to user 3
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 3, 'player1-phone3')  # add phone to user 3
 
     db.users.create_user(
         connection, 'player2', 'player2-email1', 'player2-phone1')  # user_id = 4
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 4, 'player2-phone2')  # add phone to user 4
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 4, 'player2-phone3')  # add phone to user 4
 
     db.users.create_user(
         connection, 'player3', 'player3-email1', 'player3-phone1')  # user_id = 5
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 5, 'player3-phone2')  # add phone to user 5
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 5, 'player3-phone3')  # add phone to user 5
 
     db.users.create_user(
         connection, 'player4', 'player4-email1', 'player4-phone1')  # user_id = 6
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 6, 'player4-phone2')  # add phone to user 6
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 6, 'player4-phone3')  # add phone to user 6
 
     db.users.create_user(
         connection, 'player5', 'player5-email1', 'player5-phone1')  # user_id = 7
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 7, 'player5-phone2')  # add phone to user 7
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 7, 'player5-phone3')  # add phone to user 7
 
     db.users.create_user(
         connection, 'player6', 'player6-email1', 'player6-phone1')  # user_id = 8
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 8, 'player6-phone2')  # add phone to user 8
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 8, 'player6-phone3')  # add phone to user 8
 
     db.users.create_user(
         connection, 'player7', 'player7-email1', 'player7-phone1')  # user_id = 9
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 9, 'player7-phone2')  # add phone to user 9
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 9, 'player7-phone3')  # add phone to user 9
 
     db.users.create_user(
         connection, 'player8', 'player8-email1', 'player8-phone1')  # user_id = 10
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 10, 'player8-phone2')  # add phone to user 10
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 10, 'player8-phone3')  # add phone to user 10
 
     db.users.create_user(
         connection, 'player9', 'player9-email1', '16613104788')  # user_id = 11
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 11, 'player9-phone2')  # add phone to user 11
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 11, 'player9-phone3')  # add phone to user 11
 
     db.users.create_user(
         connection, 'player10', 'player10-email1', 'player10-phone1')  # user_id = 12
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 12, 'player10-phone2')  # add phone to user 12
-    db.users.add_phone_number(
+    db.users.add_phone_number_to_user(
         connection, 12, 'player10-phone3')  # add phone to user 12
 
     db.teams.create_team(connection, 'team1', 1)  # team_id = 1
     db.teams.create_team(connection, 'team2', 2)  # team_id = 2
 
-    db.teams.add_to_team(connection, 4, 1)  # add player 2 to team 1
-    db.teams.add_to_team(connection, 5, 1)  # add player 3 to team 1
-    db.teams.add_to_team(connection, 6, 1)  # add player 4 to team 1
-    db.teams.add_to_team(connection, 7, 1)  # add player 5 to team 1
-    db.teams.add_to_team(connection, 8, 1)  # add player 6 to team 1
-    db.teams.add_to_team(connection, 9, 1)  # add player 7 to team 1
-    db.teams.add_to_team(connection, 3, 2)  # add player 1 to team 2
-    db.teams.add_to_team(connection, 4, 2)  # add player 2 to team 2
-    db.teams.add_to_team(connection, 8, 2)  # add player 6 to team 2
-    db.teams.add_to_team(connection, 9, 2)  # add player 7 to team 2
-    db.teams.add_to_team(connection, 10, 2)  # add player 8 to team 2
-    db.teams.add_to_team(connection, 11, 2)  # add player 9 to team 2
-    db.teams.add_to_team(connection, 12, 2)  # add player 10 to team 2
+    db.teams.add_user_to_team(connection, 4, 1)  # add player 2 to team 1
+    db.teams.add_user_to_team(connection, 5, 1)  # add player 3 to team 1
+    db.teams.add_user_to_team(connection, 6, 1)  # add player 4 to team 1
+    db.teams.add_user_to_team(connection, 7, 1)  # add player 5 to team 1
+    db.teams.add_user_to_team(connection, 8, 1)  # add player 6 to team 1
+    db.teams.add_user_to_team(connection, 9, 1)  # add player 7 to team 1
+    db.teams.add_user_to_team(connection, 3, 2)  # add player 1 to team 2
+    db.teams.add_user_to_team(connection, 4, 2)  # add player 2 to team 2
+    db.teams.add_user_to_team(connection, 8, 2)  # add player 6 to team 2
+    db.teams.add_user_to_team(connection, 9, 2)  # add player 7 to team 2
+    db.teams.add_user_to_team(connection, 10, 2)  # add player 8 to team 2
+    db.teams.add_user_to_team(connection, 11, 2)  # add player 9 to team 2
+    db.teams.add_user_to_team(connection, 12, 2)  # add player 10 to team 2
 
-    db.teams.change_permission_level(
+    db.teams.change_users_permission_level_for_team(
         connection, 6, 1, 1)  # make player 4 admin on team 1
-    db.teams.change_permission_level(
+    db.teams.change_users_permission_level_for_team(
         connection, 7, 1, 1)  # make player 5 admin on team 1
-    db.teams.change_permission_level(
+    db.teams.change_users_permission_level_for_team(
         connection, 12, 2, 1)  # make player 10 admin on team 2
 
     db.groups.create_group(connection, 'group1', 1)  # group_id = 3
@@ -192,77 +192,77 @@ def create_test_data():
         connection, 4, 2, 'group2-team2-message3')  # send message to group 2 on team 2
 
     # send message to player 3 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 5, 1, 'player3-team1-message1')
     # send message to player 3 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 5, 1, 'player3-team1-message2')
     # send message to player 3 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 5, 1, 'player3-team1-message3')
     # send message to player 4 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 6, 1, 'player4-team1-message1')
     # send message to player 4 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 6, 1, 'player4-team1-message2')
     # send message to player 4 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 6, 1, 'player4-team1-message3')
     # send message to player 5 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 7, 1, 'player5-team1-message1')
     # send message to player 5 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 7, 1, 'player5-team1-message2')
     # send message to player 5 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 7, 1, 'player5-team1-message3')
     # send message to player 6 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 8, 1, 'player6-team1-message1')
     # send message to player 6 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 8, 1, 'player6-team1-message2')
     # send message to player 6 on team 1
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 8, 1, 'player6-team1-message3')
 
     # send message to player 9 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 11, 1, 'player3-team2-message1')
     # send message to player 9 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 11, 1, 'player3-team2-message2')
     # send message to player 9 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 11, 1, 'player3-team2-message3')
     # send message to player 10 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 12, 1, 'player4-team2-message1')
     # send message to player 10 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 12, 1, 'player4-team2-message2')
     # send message to player 10 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 12, 1, 'player4-team2-message3')
     # send message to player 1 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 3, 1, 'player5-team2-message1')
     # send message to player 1 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 3, 1, 'player5-team2-message2')
     # send message to player 1 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 3, 1, 'player5-team2-message3')
     # send message to player 8 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 10, 1, 'player8-team2-message1')
     # send message to player 8 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 10, 1, 'player8-team2-message2')
     # send message to player 8 on team 2
-    db.messages.create_message(
+    db.messages.create_direct_message(
         connection, 10, 1, 'player8-team2-message3')
 
     return 'successfully created data'
@@ -270,5 +270,5 @@ def create_test_data():
 
 @testbp.route('/test')
 def test():
-    result = db.groups.create_group(connection, 'test', 1)
+    result = db.messages.get_groups_messages(connection, 3)
     return jsonify(result), 200
