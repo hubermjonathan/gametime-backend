55837076b2558902f21c2bbb06bf3e99bdece920
Ubuntu <ubuntu@ip-172-31-84-5.ec2.internal>
2020-11-03 21:24:28 +0000

    updated script

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ git checkout -- .
 git checkout master
 git pull origin master
 pip3 install -r requirements.txt
-setsid nohup flask run --host 0.0.0.0 --port 8080 &
+setsid nohup flask run --host 0.0.0.0 --port 8080 --cert /etc/letsencrypt/live/gametime-server.hubermjonathan.com/fullchain.pem --key /etc/letsencrypt/live/gametime-server.hubermjonathan.com/privkey.pem &
 
