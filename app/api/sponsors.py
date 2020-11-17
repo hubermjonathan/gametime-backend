0907625aaed9d8fad246ba4bf10f62e24f5e4bbc
Jon Huber <hubermjonathan@gmail.com>
2020-11-16 16:58:54 -0800

    added contact endpoint

--- app/api/sponsors.py
+++ app/api/sponsors.py
@@ -5,7 +5,7 @@ from flask_login import login_required, current_user
 import boto3
 from os import environ
 import base64
-from ..db import sponsors as db
+from .. import db
 from . import schema
 from .. import auth
 
@@ -16,6 +16,13 @@ s3 = boto3.client(
     aws_secret_access_key=environ.get('AWS_SECRET_ACCESS_KEY')
 )
 
+ses = boto3.client(
+    'ses',
+    aws_access_key_id=environ.get('AWS_ACCESS_KEY'),
+    aws_secret_access_key=environ.get('AWS_SECRET_ACCESS_KEY'),
+    region_name='us-east-1'
+)
+
 sponsorsbp = Blueprint('sponsorsbp', __name__)
 
 
@@ -24,7 +31,7 @@ def sponsors_get():
     if request.method == 'GET':
         team_id = request.args.get('team_id')
 
-        message, error, data = db.get_sponsors_for_team(
+        message, error, data = db.sponsors.get_sponsors_for_team(
             team_id)
 
         if error:
@@ -55,7 +62,7 @@ def sponsors():
         if not auth.isOwner(current_user.user_id, team_id):
             return jsonify({'message': 'invalid permissions'}), 400
 
-        message, error, data = db.create_sponsor(
+        message, error, data = db.sponsors.create_sponsor(
             team_id, name, picture)
 
         if error:
@@ -80,11 +87,56 @@ def sponsors():
             Key=f'{sponsor_id}.jpeg'
         )
 
-        message, error, data = db.remove_sponsor(sponsor_id)
+        message, error, data = db.sponsors.remove_sponsor(sponsor_id)
 
         if error:
             return jsonify({'message': message}), 500
 
         return jsonify(data), 200
+    else:
+        return jsonify({'message': 'method not allowed'}), 405
+
+
+@sponsorsbp.route('/sponsors/contact', methods=['POST'])
+def sponsors_contact():
+    if request.method == 'POST':
+        try:
+            body = request.get_json()
+            validate(body, schema=schema.sponsors_contact_schema)
+
+            team_id = body['team_id']
+            subject = body['subject']
+            body = body['body']
+        except Exception:
+            return jsonify({'message': 'invalid body provided'}), 400
+
+        message, error, data = db.teams.get_teams_coach(team_id)
+        print(data)
+
+        if error:
+            return jsonify({'message': message}), 500
+
+        response = ses.send_email(
+            Source='gametimefundraising@gmail.com',
+            Destination={
+                'ToAddresses': [
+                    data['email']
+                ]
+            },
+            Message={
+                'Subject': {
+                    'Data': subject,
+                    'Charset': 'utf-8'
+                },
+                'Body': {
+                    'Text': {
+                        'Data': body,
+                        'Charset': 'utf-8'
+                    }
+                }
+            }
+        )
+
+        return jsonify({'message': 'successfully sent message'}), 200
     else:
         return jsonify({'message': 'method not allowed'}), 405
