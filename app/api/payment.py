9c6d985ec4f5fba564cb596f7ee618bded30c7e3
Jay Rixie <jayrixie@gmail.com>
2020-11-29 15:13:20 -0600

    Completed payment, adding bank account

--- app/api/payment.py
+++ app/api/payment.py
@@ -18,25 +18,31 @@ stripe.api_key = environ.get('STRIPE_PRIVATE_KEY')
 def create_checkout_session():
   body = request.get_json()
 
-  item_list = body['item_ids']
+  try:
+    item_list = body['item_ids']
+  
 
-  line_items = []
-  for item in item_list:
-    itemId = item[0]
-    itemQ = item[1]
+    line_items = []
+    for item in item_list:
+      itemId = item[0]
+      itemQ = item[1]
 
-    itemData = store.get_item(itemId)[2]
-    line_items.append(
-      {
-        'price_data': {
-          'currency': 'usd',
-          'product_data': {
-            'name': itemData['name'],
+      itemData = store.get_item(itemId)[2]
+      line_items.append(
+        {
+          'price_data': {
+            'currency': 'usd',
+            'product_data': {
+              'name': itemData['name'],
+            },
+            'unit_amount_decimal': (int)(itemData['price']*100), #convert to cents
           },
-          'unit_amount_decimal': (int)(itemData['price']*100), #convert to cents
-        },
-        'quantity': itemQ,
-      })
+          'quantity': itemQ,
+        })
+  except KeyError:
+    return "missing field in body", 400
+  except Exception as e:
+    return str(e), 500
 
   try:
     session = stripe.checkout.Session.create(
@@ -45,11 +51,14 @@ def create_checkout_session():
       mode='payment',
       success_url=body['success_url'],
       cancel_url=body['cancel_url'],
+      customer_email=body['email']
     )
 
     return jsonify(id=session.id), 200
   except stripe.error.InvalidRequestError as e:
     return ' '.join(str(e).split(' ')[2:]), 400
+  except KeyError:
+    return "missing field in body", 400
   except Exception as e:
     return str(e), 500
 
@@ -73,10 +82,28 @@ def create_donation_session():
       mode='payment',
       success_url='https://example.com/success',
       cancel_url='https://example.com/cancel',
+      customer_email=body['email']
     )
 
     return jsonify(id=session.id), 200
   except stripe.error.InvalidRequestError as e:
     return ' '.join(str(e).split(' ')[2:]), 400
+  except KeyError:
+    return "missing field in body", 400
   except Exception as e:
     return str(e), 500
+
+@paymentsbp.route('/changeBankAccount', methods=['POST'])
+@login_required
+def change_bank_account():
+  body = request.get_json()
+
+  try:
+    teamId = body["team_id"]
+    accountNum = body["account_number"]
+    routingNum = body["routing_number"]
+  except:
+      return "missing field", 400
+
+  if not auth.isOwner(current_user.user_id, teamId):
+    return "Not owner of team", 401
